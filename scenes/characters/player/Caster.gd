c29e040c2693e4dbb3c25a1f32fbdbbeeb7fba30
Julian Hedén <julianheden@gmail.com>
2022-06-07 09:19:52 +0200

    ADD slice spell to Caster

--- scenes/characters/player/Caster.gd
+++ scenes/characters/player/Caster.gd
@@ -1,21 +1,28 @@
 extends "res://scenes/characters/player/Player.gd"
 
-var bubble_scene = preload("res://scenes/spells/Bubble.tscn")
 var projectile_manager_scene = preload("res://scenes/spells/ProjectileManager.tscn")
 
 func spells() -> void:
-	var bubble = bubble_scene.instance()
-	var projectile_manager = projectile_manager_scene.instance()
-	# Add bubble as shield
-	add_child(bubble)
-	add_child(projectile_manager)
+	var boulder_manager = projectile_manager_scene.instance()
+	boulder_manager.set_projectile(
+		"res://scenes/objects/projectiles/Boulder.tscn",
+		"res://resources/sprites/rockpng.png"
+	)
+	var slice_manager = projectile_manager_scene.instance()
+	slice_manager.set_projectile(
+		"res://scenes/objects/projectiles/Slice.tscn",
+		"res://resources/sprites/slice.png"
+	)
+	# Add spell managers as childs
+	add_child(boulder_manager)
+	add_child(slice_manager)
 	# Define all spells
-	spell_bindings["q"] = funcref(bubble, "activate")
-	spell_bindings["w"] = funcref(projectile_manager, "activate")
+	spell_bindings["q"] = funcref(slice_manager, "activate")
+	spell_bindings["w"] = funcref(boulder_manager, "activate")
 
 	# Update HUD with spells
 	hud.load_spellbar(
 		["q", "w"],
-		[bubble.icon(), projectile_manager.icon()],
-		[bubble.cooldown, projectile_manager.cooldown]
+		[slice_manager.icon(), boulder_manager.icon()],
+		[slice_manager.cooldown, boulder_manager.cooldown]
 	)
--- scenes/spells/ProjectileManager.gd
+++ scenes/spells/ProjectileManager.gd
@@ -1,14 +1,21 @@
 extends "res://scenes/spells/SpellManager.gd"
 
-var rock_scene = preload("res://scenes/objects/projectiles/Boulder.tscn")
+var projectile_scene: Resource
+var projectile_icon_path: String
+
+#var rock_scene = preload("res://scenes/objects/projectiles/Boulder.tscn")
 onready var persistant_objects = get_node("/root/PersistantObjects")
 onready var player = get_parent()
 
-func activate() -> void:
-	.activate()
+func set_projectile(scene_path: String, icon_path: String) -> void:
+	projectile_scene = load(scene_path)
+	projectile_icon_path = icon_path
+
+func activate(params := {}) -> void:
+	.activate(params)
 	
 	# shoot rock
-	var projectile_instance = rock_scene.instance()
+	var projectile_instance = projectile_scene.instance()
 	projectile_instance.set_network_master(player.id) # set projectile owner
 	var angle = player.get_rotation()
 	var direction = Vector2(cos(angle), sin(angle))
@@ -33,5 +40,5 @@ func icon() -> String:
 	"""
 	Returns the path to the spell icon for UI usage
 	"""
-	return "res://resources/sprites/rockpng.png"
+	return projectile_icon_path
 
